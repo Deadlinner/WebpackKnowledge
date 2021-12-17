03e754e8a6293556826b7d183baed130d4da0772
chin_hda <v-huangdea.gd@chinatelecom.cn>
2021-12-17 10:06:50 +0800

    文件夹改名

---
+++ 03.打包样式资源/webpack.config.js
@@ -0,0 +1,29 @@
+/**
+ * 所有构建工具都是基于Node.js平台运行的，模块化默认采用commonjs
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'built.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  // loader 配置
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          //use数组中loader的执行顺序：从右到左， 从下到上， 依序执行
+          'style-loader', //创建一个style标签，将JS中的样式资源插入到head中生效
+           'css-loader'// 将css文件变成commonjs的模块加载到JS中，里面内容是样式字符串
+        ]
+      }
+    ]
+  },
+  plugins: [
+
+  ],
+  mode: 'development'
+}
